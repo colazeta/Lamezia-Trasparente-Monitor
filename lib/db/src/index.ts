43d275746c5a1c3103cde261bd985879c8de1ba8
colazeta <47261299+colazeta@users.noreply.github.com>
2026-09-06 18:37:17 +0200

    db: export canonical identity helpers

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -4,6 +4,7 @@ export * from "./baselineLogic";
 export * from "./migrate";
 export * from "./migrationSafety";
 export * from "./legacySchemaCompatibility";
+export * from "./canonicalIdentity";
 export * from "./confiscatedAssetsCleanup";
 export * from "./institutional-officials";
 export * from "./organi-sync";
